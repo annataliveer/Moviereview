e63097619b03554ac0ea58f7ca5cda2c982f8d4e
annataliveer <annataliveer@yahoo.se>
2017-11-28 14:15:03 +0100

    added all files

---
+++ data.js
@@ -0,0 +1,6 @@
+export const movieData = {
+    title: "Skönheten och Odjuret",
+    imgUrl: "https://www.discshop.se/img/front_large/149814/skonheten_och_odjuret.jpg",
+    review: " An adaptation of the fairy tale about a monstrous-looking prince and a young woman who fall in love.",
+    actors: ["Emma Watson", "Dan Stevens"]
+  };
